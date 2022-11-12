51a461b3f58bd79012d03ec827efe454e8c77be9
aixoio <aixoio@yahoo.com>
2022-11-13 10:54:57 +1100

    Setup base

---
+++ nobotpage.js
@@ -0,0 +1,15 @@
+async function ajax(url, data, type, dataType) {
+
+    let result = await $.ajax(url, {
+
+        type: type,
+        data: data,
+        dataType: dataType
+
+    });
+
+    return result;
+
+}
+
+
---
+++ recaptchainfo_tmp.js
@@ -0,0 +1,8 @@
+
+// fill out the info in this file and rename it to recaptchainfo.js
+
+function getreCaptchaV3SiteKey() {
+
+    return "yourrecaptchav3sitekey"
+
+}
